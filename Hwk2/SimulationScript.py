ff7d92b6136266270a49af66d511777b74bae801
Cody Grogan <thewall907@gmail.com>
2023-09-12 10:30:54 -0600

    Finished nonlinear problem

--- Hwk2/SimulationScript.py
+++ Hwk2/SimulationScript.py
@@ -24,7 +24,7 @@ class Simulation:
         uvec: mxn vector of control inputs
     '''
     def getControlVector(self, xvec, u) -> np.ndarray:
-        u_vec = np.zeros((len(tvec),1))
+        u_vec = np.zeros((len(self.timespan),1))
         for i in range(len(self.timespan)):
             u_vec[i,:] = u(self.timespan[i], xvec[i, :]).squeeze()
         return u_vec
@@ -69,7 +69,7 @@ class Simulation:
         data: list of tuples of time, state vectors
         Titles: list of titles for each plot
 '''
-def plotResults(data: t.List[t.Tuple[np.ndarray, np.ndarray, np.ndarray]], formats: t.List[str] ) -> None:
+def plotResults(data: t.List[t.Tuple[np.ndarray, np.ndarray, np.ndarray]], formats: t.List[str], titles: t.List[str]) -> None:
     numx = data[0][1].shape[1] 
     numu =  1 if len(data[0][2].shape) <= 1 else data[0][2].shape[1]
     numplots = numx + numu
@@ -79,33 +79,74 @@ def plotResults(data: t.List[t.Tuple[np.ndarray, np.ndarray, np.ndarray]], forma
         for j in range(numx):
             # Plotting x's on subplot 
             axs[j].plot(tvec, xvec[:, j], formats[i])   
-            axs[j].set(xlabel='time', ylabel=f'x{j+1}')
+            axs[j].set(xlabel='time', ylabel=titles[j])
         
         for j in range(numx, numplots):
             # Plotting u's on subplot
             axs[j].plot(tvec, uvec[:, j-numx], formats[i])
             axs[j].set(xlabel='time', ylabel=f'u{j-numx + 1}')
-    plt.show()
 
 
-if __name__ == "__main__":
-    t0 = 0
-    dt = 0.01
-    tf = 10.0
-    g = 9.8
-    m = 1/9.8
-    l = .25
-    b = 1
-
-    u = lambda t, x: np.array([l/np.sqrt(2)])
-    A = np.array([[0, 1], [g/l/np.sqrt(2), -b/(m*l**2)]])
+def problem1(t0, dt, tf, u, f, A, linearized_x):
     eigval, eigvec = np.linalg.eig(A)
-    #x0 = eigvec[:,1].transpose()
-    x0 = np.array([np.pi/4 -.05, 0])
+    x0 = eigvec[:,1].transpose() + linearized_x
+    sim = Simulation(t0, dt, tf, u, f, x0)
+
+    data = []
+
+    tvec, xvec = sim.pythonODE(x0, u)
+    uvec = sim.getControlVector(xvec, u)
+    data.append((tvec, xvec, uvec))
+
+    plotResults(data, ['b', 'r:'], ['theta', 'theta_dot'])
+
+def problem2(t0, dt, tf, u, f, A, linearized_x):
+    eigval, eigvec = np.linalg.eig(A)
+    x0 = eigvec[:,0].transpose() + linearized_x
+    sim = Simulation(t0, dt, tf, u, f, x0)
+
+    data = []
+
+    tvec, xvec = sim.pythonODE(x0, u)
+    uvec = sim.getControlVector(xvec, u)
+    data.append((tvec, xvec, uvec))
+
+    plotResults(data, ['b', 'r:'], ['theta', 'theta_dot'])
+
+
+def problem3(t0, dt, tf, u, f, A, linearized_x):
+    x0 = np.array([-.05, 0]) + linearized_x
+    sim = Simulation(t0, dt, tf, u, f, x0)
+
+    data = []
+
+    tvec, xvec = sim.pythonODE(x0, u)
+    uvec = sim.getControlVector(xvec, u)
+    data.append((tvec, xvec, uvec))
+
+    plotResults(data, ['b', 'r:'], ['theta', 'theta_dot'])
+
+def sanityCheck(t0, dt, tf, u, f, A, linearized_x):
+    x0 = np.array([0, 0]) + linearized_x
+    sim = Simulation(t0, dt, tf, u, f, x0)
+
+    data = []
+
+    tvec, xvec = sim.pythonODE(x0, u)
+    uvec = sim.getControlVector(xvec, u)
+    data.append((tvec, xvec, uvec))
+
+    plotResults(data, ['b', 'r:'], ['theta', 'theta_dot'])
+
+
+
+def linearProblem(t0, dt, tf, g, m , l , b):
+    u = lambda t, x: np.array([-l/np.sqrt(2)])
+    A = np.array([[0, 1], [g/l/np.sqrt(2), -b/(m*l**2)]])
 
     linearized_x = np.array([np.pi/4, 0])
 
-    linearized_u = np.array([l/np.sqrt(2)])
+    linearized_u = np.array([-l/np.sqrt(2)])
     '''
         Calculates the state dynamics using the current time, state, and control vector
     '''
@@ -117,16 +158,44 @@ if __name__ == "__main__":
         first = A@delta_x
         second = B@delta_u
         xdot = first + second
-        ret = xdot.transpose()
         return xdot.transpose()
+    
+    problem1(t0, dt, tf, u, f, A, linearized_x)
+    problem2(t0, dt, tf, u, f, A, linearized_x)
+    problem3(t0, dt, tf, u, f, A, linearized_x)
 
 
-    sim = Simulation(t0, dt, tf, u, f, x0)
+def nonLinearProblem(t0, dt, tf, g, m , l , b):
+    u = lambda t, x: np.array([-l/np.sqrt(2)])
+    A = np.array([[0, 1], [g/l/np.sqrt(2), -b/(m*l**2)]])
+    linearized_x = np.array([np.pi/4, 0])   
 
-    data = []
+    def f(t, x, u):
+        B = np.array([[0], [1/(m*l**2)]])
+        uvec = u(t, x)
 
-    tvec, xvec = sim.pythonODE(x0, u)
-    uvec = sim.getControlVector(xvec, u)
-    data.append((tvec, xvec, uvec))
+        first = np.array([x[1], (g/l)*np.sin(x[0]) - (b/(m*l**2))*x[1]])
+        second = B@uvec.transpose()
 
-    plotResults(data, ['b', 'r:'])
+        return (first + second).transpose()
+    
+    problem1(t0, dt, tf, u, f, A, linearized_x)
+    problem2(t0, dt, tf, u, f, A, linearized_x)
+    problem3(t0, dt, tf, u, f, A, linearized_x)
+
+    
+ 
+
+if __name__ == "__main__":
+    t0 = 0
+    dt = 0.01
+    tf = 10.0
+    g = 9.8
+    m = 1/9.8
+    l = .25
+    b = 1
+    
+    linearProblem(t0, dt, tf, g, m, l, b)
+
+    nonLinearProblem(t0, dt, tf, g, m, l, b)
+    plt.show()
